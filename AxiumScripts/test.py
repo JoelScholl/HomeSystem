e911f290c8073a0ab2359bfe9dc0dffb333c3ac4
JoelScholl <joel@scholls.ch>
2022-07-23 00:16:38 +0200

    V1.1

--- AxiumScripts/test.py
+++ AxiumScripts/test.py
@@ -1,3 +1,6 @@
 import axiumctrl as ax
+import requests as rq
 
-ax.chk_amp(0)
+#ax.chk_amp(0)
+r = rq.get("http://192.168.1.32/Web/Handler.php?page=home&action=read",headers={},data={},timeout=3)
+print(r)
