43e893da545493352b85601cfc2be3ec677e3bcd
Joël Scholl <joscholl@student.ethz.ch>
2022-07-22 21:48:07 +0200

    Introduced HM API

--- Axium Scripts/axiumctrl.py
+++ Axium Scripts/axiumctrl.py
@@ -1,12 +1,49 @@
 import requests
 
+# -------------------- Request wrapper ----------------------------------------------- 
+
+ def try_request(type,url,headers,data = None ,timeout:int = 3 ,loop:bool = False):
+
+	if(type=='get'):
+				r = requests.get(url,headers=headers,data=data,timeout=timeout)
+		while(loop):
+			try:
+				r = requests.get(url,headers=headers,data=data,timeout=timeout)
+				break
+			except:
+				continue
+
+	if(type=='post'):
+				r = requests.post(url,headers=headers,data=data,timeout=timeout)
+		while(loop):
+			try:
+				r = requests.post(url,headers=headers,data=data,timeout=timeout)
+				break
+			except:
+				continue
+	return
+
+# ------------------------------------------------------------------------------------
+
+
+# --------------------------- Homematic API ------------------------------------------
+def call(cgi,params):
+	hm_url = "http://192.168.1.41/addons/xmlapi/"+cgi+".cgi"+
+
+def get_sysvar(id:str):
+
+
+#-------------------------------------------------------------------------------------
+
+
+# ----------------------------  Axium API  -------------------------------------------
 def send_cmd(payload):
 	id='3764335675'
 	page = "http://192.168.1.58/axium.cgi?id="+id
 	headers = {'Host': '192.168.1.58','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0','Accept': '*/*','Accept-Language': 'en-US,en;q=0.5','Accept-Encoding': 'gzip, deflate','Content-Type': 'application/x-axium','Content-Length': '8','Origin': 'http://192.168.1.58','Connection': 'keep-alive','Referer': 'http://192.168.1.58/zone.html?id=3764335675'}
 	r = requests.post(page,headers=headers,data=payload,timeout=1)
 
-def zone(zone:str, state:str):
+def set_zone(zone:str, state:str):
 	payload = '01'+zone_id(zone)+'0'+state_id(state)+'\r\n'
 	send_cmd(payload)
 
@@ -19,9 +56,6 @@ def vol(zone='4', vol='10'):
 	payload = '04'+zone+vol+'\r\n'
 	send_cmd(payload)
 
-def try_request(url,headers,data,timeout:int,loop:bool):
-	return
-
 def chk_beast():
 	beast_url = "http://192.168.1.12/index.php"
 	headers = {'Host':'192.168.1.31','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0','Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8','Accept-Language':'en-US,en;q=0.5','Accept-Encoding':'gzip, deflate','Connection':'keep-alive','Referer':'http://192.168.1.31/protect/power.htm','Upgrade-Insecure-Requests':'1','Authorization':'Basic YWRtaW46YXZy'}
@@ -33,6 +67,8 @@ def chk_beast():
 	except:
 		print("Beast is not running!")
 		return False
+
+
 
 def chk_amp(amp:int):
 	chk_amp_url = "http://192.168.1.3"+str((2+2*amp))+"/Web/Handler.php?page=home&action=read"
@@ -56,3 +92,5 @@ def torus(state:str):
 	payload = {}
 	headers = {'Host':'192.168.1.31','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0','Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8','Accept-Language':'en-US,en;q=0.5','Accept-Encoding':'gzip, deflate','Connection':'keep-alive','Referer':'http://192.168.1.31/protect/power.htm','Upgrade-Insecure-Requests':'1','Authorization':'Basic YWRtaW46YXZy'}
 	r = requests.get(torus_url,headers=headers,data=payload,timeout=3)
+
+# ------------------------------------------------------------------------------------
