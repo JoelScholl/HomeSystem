dea3f08369769dd8eeaf0bef16871002033ed3fc
JoelScholl <joel@scholls.ch>
2022-07-24 20:28:45 +0100

    V2.0 First test using cronjobs

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import HomeSystem.audio as audio
 import HomeSystem.hm as hm
-
+import xml.etree.ElementTree as ET
 
 # state = hm.get_state('5263','5293','5297')[0].attrib['value']
 # state2 = hm.get_state_val('5263','5293','5297')
