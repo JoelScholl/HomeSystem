c2e65f0bbc9a02698aaefa1675aeb98a0ac55cb1
JoelScholl <joel@scholls.ch>
2022-07-25 19:47:48 +0200

    V2.1

--- alloff.py
+++ alloff.py
@@ -1,11 +1,15 @@
 import HomeSystem.audio as audio
 import HomeSystem.hm as hm
 import time
+import logging
+
+logging.basicConfig(filename="log.txt", level=logging.DEBUG)
 
 def soft_shutdown():
     if(audio.chk_beast()):
         print("Shutting down Beast!")
         audio.set_beast()
+        logging.debug("Beast killed.")
 
     if(audio.get_amp(0)):
         print("Shutting down Amp 1")
@@ -24,6 +28,13 @@ auto_chk = True if (hm.get_sysvar('50807')['value_text']=='auto') else False
 time_chk = True if (((time.localtime()[3] == int(shutdown_time[0:2])) and (abs(time.localtime()[4]-int(shutdown_time[3:5]))<10))) else False
 master_chk = False if hm.get_state_val('5263','5293','5297') == 'true' else True
 
+logging.debug("Shutdown Time is set to: "+shutdown_time)
+logging.debug("Current time is: "+time.localtime()[3])
+logging.debug("Time check is thus: "+time_chk)
+logging.debug("Master Check is "+master_chk)
+
 if(auto_chk and master_chk):
+    logging.debug("Autocheck and Mastercheck passed!")
     if(time_chk or (time.localtime()[3]==1)):
+        logging.debug("Timecheck passed, running soft shutdown")
         soft_shutdown()
