8b0e60077bb847ab65aad35f4802af5410aa92df
JoelScholl <joel@scholls.ch>
2022-07-25 20:04:33 +0200

    V2.2 Fixing on RPi

--- HomeSystem/audio.py
+++ HomeSystem/audio.py
@@ -18,7 +18,7 @@ class zone():
         self.src = None
         self.vol = None
         self.Mute = None
-    
+
     def update(self):
         return
 
@@ -40,7 +40,7 @@ def src_id(src:str):
                 '0A':'0A','A':'0A','Musik Marcel':'0A', #Musik Marcel:  0A
                 '0B':'0B','B':'0B','wohnen':'0B','TV Wohnen':'0B', #TV Wohnen: 0B
                 '21':'21','radio1':'21','iRadio 1':'21', #iRadio 1: 21
-                '12':'12','radio1':'12','iRadio 2':'12'#iRadio 2: 12
+                '12':'12','radio1':'12','iRadio 2':'12' #iRadio 2: 12
                 }
     return src_dict[src]
 
@@ -53,8 +53,7 @@ def set_zone(zone:str, state:str):
 def set_src(zone:str, src:str):
     payload = '03'+zone_id(zone)+src_id(src)+'\r\n'
     send_cmd(payload)
-    
-    
+
 def set_vol(zone:str='4', vol:str='10'):
     payload = '04'+zone+vol+'\r\n'
     send_cmd(payload)
@@ -65,10 +64,10 @@ def get_beast():
     try:
         r = www.get(beast_url,headers=headers,data={},timeout=2)
         if(r.ok):
-            print("Beast is running!")
+            logging.debug("Beast is running!")
             return True
     except:
-        print("Beast is not running!")
+        logging.debug("Beast is not running!")
         return False
 
 def get_amp(amp:int):
@@ -85,11 +84,11 @@ def get_amp(amp:int):
 def set_amp(amp:int,state:bool):
     func_amp_url = "http://192.168.1.3"+str((2+2*amp))+"/Web/Handler.php?page=home&action=write&name=cur-standby&value="+str(int(state))+"&r=0.666583746119017"
     r = www.get(func_amp_url,headers={},data={},timeout=1)
-    
+
 def set_beast():
     beast_off = "http://192.168.1.12/shared/taskmanager.php?task=system&cmd=stop"
     r = www.get(beast_off,headers={},data={},timeout=1)
-    logging.debug("Response is: "+r.text)
+    logging.debug("Response is: "+str(r.text))
 
 
 def set_torus(state:str):
@@ -97,3 +96,4 @@ def set_torus(state:str):
     payload = {}
     headers = {'Host':'192.168.1.31','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0','Accept':'text/www,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8','Accept-Language':'en-US,en;q=0.5','Accept-Encoding':'gzip, deflate','Connection':'keep-alive','Referer':'http://192.168.1.31/protect/power.htm','Upgrade-Insecure-Requests':'1','Authorization':'Basic YWRtaW46YXZy'}
     r = www.get(torus_url,headers=headers,data=payload,timeout=3)
+    logging.debug("Response is: "+str(r.text))
--- alloff.py
+++ alloff.py
@@ -6,7 +6,7 @@ import logging
 logging.basicConfig(filename="log.txt", level=logging.DEBUG)
 
 def soft_shutdown():
-    if(audio.chk_beast()):
+    if(audio.get_beast()):
         print("Shutting down Beast!")
         audio.set_beast()
         logging.debug("Beast killed.")
@@ -28,10 +28,13 @@ auto_chk = True if (hm.get_sysvar('50807')['value_text']=='auto') else False
 time_chk = True if (((time.localtime()[3] == int(shutdown_time[0:2])) and (abs(time.localtime()[4]-int(shutdown_time[3:5]))<10))) else False
 master_chk = False if hm.get_state_val('5263','5293','5297') == 'true' else True
 
-logging.debug("Shutdown Time is set to: "+shutdown_time)
-logging.debug("Current time is: "+time.localtime()[3])
-logging.debug("Time check is thus: "+time_chk)
-logging.debug("Master Check is "+master_chk)
+logging.debug("Shutdown Time is set to: "+str(shutdown_time))
+logging.debug("Current time is: "+str(time.localtime()[3]))
+logging.debug("Time check is thus: "+str(time_chk))
+logging.debug("Master Check is "+str(master_chk))
+
+master_chk = True
+time_chk = True
 
 if(auto_chk and master_chk):
     logging.debug("Autocheck and Mastercheck passed!")
