cc487654d5a77c4589d966bfc2c3c343082b6cc5
Joël Scholl <joscholl@student.ethz.ch>
2022-07-28 19:38:16 +0200

    V2.3 Revised Code - Testing

--- HomeSystem/www.py
+++ HomeSystem/www.py
@@ -2,15 +2,18 @@ import requests
 
 def get(url:str,headers = None, data = None, timeout:int = 3, loop:bool = False):
 	while(loop):
-		#print("Looping")
+		print("Looping")
 		try:
 			r = requests.get(url,headers=headers,data=data,timeout=timeout)
+			print("Going twice with URL:"+url)
 			return r
 		except:
+			print("Except")
 			continue
-			#print("Except")
+			
 	try:
 		r = requests.get(url,headers=headers,data=data,timeout=timeout)
+		print("Going once with URL:"+url)
 		return r
 	except:
 		print("Webserver at "+url+"refused to connect.")
--- test.py
+++ test.py
@@ -2,12 +2,4 @@ import HomeSystem.audio as audio
 import HomeSystem.hm as hm
 import xml.etree.ElementTree as ET
 
-# state = hm.get_state('5263','5293','5297')[0].attrib['value']
-# state2 = hm.get_state_val('5263','5293','5297')
 
-# print(state)
-# print(state2)
-# print(audio.get_amp(0))
-# print(audio.get_amp(1))
-# print(audio.get_amp(2))
-# print(audio.get_beast())
