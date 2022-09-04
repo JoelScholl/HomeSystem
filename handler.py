18ed35790200155a2904cc72e027a9250c6e0cc4
JoelScholl <joel@scholls.ch>
2022-09-04 18:06:48 +0200

    V2.9.3

--- handler.py
+++ handler.py
@@ -13,13 +13,13 @@ def torus_shutdown_time():
     return '<h3>I choose you!</h3>'
 
 #Catch stray routes
-@app.route('/sysvar', methods=['GET'])
-def mainroute():
-    id = request.args.get('id')
-    name = request.args.get('name')
-    value = hm.VarToString(id,request.args.get('value'))
-    print("ID:",id)
-    print("Name:",name)
-    print("Value:",value)
-    return '<h3>ID: {}</h3><h3>Name: {}</h3><h3>Value: {}</h3>'.format(escape(id),escape(name),escape(value))
+# @app.route('/sysvar', methods=['GET'])
+# def mainroute():
+#     id = request.args.get('id')
+#     name = request.args.get('name')
+#     value = hm.VarToString(id,request.args.get('value'))
+#     print("ID:",id)
+#     print("Name:",name)
+#     print("Value:",value)
+#     return '<h3>ID: {}</h3><h3>Name: {}</h3><h3>Value: {}</h3>'.format(escape(id),escape(name),escape(value))
 
