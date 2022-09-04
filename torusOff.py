de8f581ca7a3e1aa612392781bd08968f9b39149
JoelScholl <joel@scholls.ch>
2022-09-04 23:20:27 +0200

    V3.1.2 Debugging

--- torusOff.py
+++ torusOff.py
@@ -27,20 +27,21 @@ def soft_shutdown():
 #torus_shutdown_time = hm.getSysVar('49106')['value_text']
 #beast_shutdown_time = hm.getSysVar('')
 
-auto_chk = True if (hm.getSysVar('50807')['value_text']=='auto') else False
+
 #time_chk = True if (((time.localtime()[3] == int(torus_shutdown_time[0:2])) and (abs(time.localtime()[4]-int(torus_shutdown_time[3:5]))<50))) else False
 #time_chk = True
 #beast_auto_chk = True if (hm.getSysVar('52847')['value']=='true') else False
 #beast_time_chk =
 
-joel_chk = False if hm.getStateVal('5263','5293','5297') == 'true' else True
-tv_chk = False if hm.getStateVal('5141','5173','5179') == 'true' else True
-rkport_chk = False if hm.getStateVal('5336','5379','5383') == 'true' else True
+auto_chk = True if (hm.getSysVar('50807')['value_text']=='auto') else False
+joel_chk = False if (hm.getStateVal('5263','5293','5297') == 'true') else True
+tv_chk = False if (hm.getStateVal('5141','5173','5179') == 'true') else True
+rkport_chk = False if (hm.getStateVal('5336','5379','5383') == 'true') else True
 
 print("Shutdown Time:",torus_shutdown_time,"Time_Chk",time_chk,"joel_chk",joel_chk,"tv_chk",tv_chk,"rkport_chk",rkport_chk)
 
 def main():
-    if(time_chk and auto_chk and joel_chk and tv_chk and rkport_chk):
+    if(auto_chk and joel_chk and tv_chk and rkport_chk):
         print("All checks passed!")
         soft_shutdown()
 
