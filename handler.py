7e64ea545ecd0f06329d9ea572073f21e55a89be
JoelScholl <joel@scholls.ch>
2022-09-04 18:12:42 +0200

    V2.9.5

--- handler.py
+++ handler.py
@@ -8,18 +8,18 @@ from Lib.scheduler import *
 app = Flask(__name__)
 
 #Manage torus_shutdown_time changes
-@app.route('/sysvar?id=52798')
+@app.route('/')
 def torus_shutdown_time():
-    return '<h3>I choose you!</h3>'
+    return '<h3>Currently only /sysvar is supported.</h3>'
 
 #Catch stray routes
-# @app.route('/sysvar', methods=['GET'])
-# def mainroute():
-#     id = request.args.get('id')
-#     name = request.args.get('name')
-#     value = hm.VarToString(id,request.args.get('value'))
-#     print("ID:",id)
-#     print("Name:",name)
-#     print("Value:",value)
-#     return '<h3>ID: {}</h3><h3>Name: {}</h3><h3>Value: {}</h3>'.format(escape(id),escape(name),escape(value))
+@app.route('/sysvar', methods=['GET'])
+def mainroute():
+    id = request.args.get('id')
+    name = request.args.get('name')
+    value = hm.VarToString(id,request.args.get('value'))
+    print("ID:",id)
+    print("Name:",name)
+    print("Value:",value)
+    return '<h3>ID: {}</h3><h3>Name: {}</h3><h3>Value: {}</h3>'.format(escape(id),escape(name),escape(value))
 
