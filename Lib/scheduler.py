09d0e542ee8e199a44584edaa62037667df9fca2
JoelScholl <joel@scholls.ch>
2022-09-09 11:17:44 +0200

    V3.2.2 Cleaning up bugs

--- Lib/scheduler.py
+++ Lib/scheduler.py
@@ -5,9 +5,10 @@ def sudo(command):
     return os.system(f"sudo bash -c '{command}'")
 
 def StrToCron(time:str):
-    hour = time[:2]
-    minute = time[3:]
-    return (bool(int(minute[0]))*minute[0])+minute[1]+" "+(bool(int(hour[0]))*hour[0])+hour[1]
+    hour = time.split(':')[0].lstrip('0')
+    minute = time.split(':')[1].lstrip('0')
+    print(minute+' '+hour)
+    return minute+' '+hour
 
 def schedule(script:str,time:str):
     path = '/etc/cron.d/'+script
